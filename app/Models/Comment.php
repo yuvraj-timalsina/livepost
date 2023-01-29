f31d42e796d16256a5f3bb558b9157295d858513
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-01-29 16:08:27 +0545

    Defining Relationship

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -1,15 +1,22 @@
 <?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Comment extends Model
-{
-    use HasFactory;
     
-    protected $casts = [
-        'body' => 'array',
-    ];
-}
+    namespace App\Models;
+    
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Relations\BelongsTo;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    
+    class Comment extends Model
+    {
+        use HasFactory;
+        
+        protected $casts = [
+            'body' => 'array',
+        ];
+        
+        
+        public function post(): BelongsTo
+        {
+            return $this->belongsTo(Post::class, 'post_id');
+        }
+    }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,15 +1,29 @@
 <?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Post extends Model
-{
-    use HasFactory;
     
-    protected $casts = [
-        'body' => 'array',
-    ];
-}
+    namespace App\Models;
+    
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Relations\HasMany;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+    class Post extends Model
+    {
+        use HasFactory;
+        
+        protected $casts = [
+            'body' => 'array',
+        ];
+        
+        
+        public function comments(): HasMany
+        {
+            return $this->hasMany(Comment::class, 'post_id');
+        }
+        
+        
+        public function users(): BelongsToMany
+        {
+            return $this->belongsToMany(User::class, 'post_user', 'post_id', 'user_id');
+        }
+    }
