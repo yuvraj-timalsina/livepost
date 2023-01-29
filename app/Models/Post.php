fb955dfe6ed40215e4a92851977f053a2f540fc9
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-01-29 16:33:07 +0545

    Accessor & Mutator

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,10 +3,11 @@
     namespace App\Models;
     
     use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Casts\Attribute;
     use Illuminate\Database\Eloquent\Relations\HasMany;
     use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-
+    
     class Post extends Model
     {
         use HasFactory;
@@ -14,6 +15,15 @@
         protected $casts = [
             'body' => 'array',
         ];
+        
+        
+        public function title(): Attribute
+        {
+            return new Attribute(
+                get: fn($value) => strtoupper($value),
+                set: fn($value) => strtolower($value),
+            );
+        }
         
         
         public function comments(): HasMany
