1bfb6e118b3f95b070734c969ea37494ec884a26
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-01-30 12:28:33 +0545

    User Controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,74 @@
+<?php
+    
+    namespace App\Http\Controllers;
+    
+    use App\Models\User;
+    use Illuminate\Http\Request;
+    use Illuminate\Http\Response;
+    use function response;
+    
+    class UserController extends Controller
+    {
+        /**
+         * Display a listing of the resource.
+         *
+         * @return Response
+         */
+        public function index(): Response
+        {
+            return response([
+                'data' => 'John Doe',
+            ]);
+        }
+        
+        
+        /**
+         * Store a newly created resource in storage.
+         *
+         * @param Request $request
+         * @return Response
+         */
+        public function store(Request $request): Response
+        {
+            return new Response('User created', 201);
+        }
+        
+        
+        /**
+         * Display the specified resource.
+         *
+         * @param User $user
+         * @return Response
+         */
+        public function show(User $user): Response
+        {
+            return response([
+                'data' => $user,
+            ]);
+        }
+        
+        
+        /**
+         * Update the specified resource in storage.
+         *
+         * @param Request $request
+         * @param User $user
+         * @return Response
+         */
+        public function update(Request $request, User $user): Response
+        {
+            return new Response('User updated', 201);
+        }
+    
+    
+        /**
+         * Remove the specified resource from storage.
+         *
+         * @param User $user
+         * @return Response
+         */
+        public function destroy(User $user): Response
+        {
+            return new Response('User deleted', 201);
+        }
+    }
--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+    use App\Http\Controllers\UserController;
+    
+    /*
+    |--------------------------------------------------------------------------
+    | API Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register API routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | is assigned the "api" middleware group. Enjoy building your API!
+    |
+    */
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{user}', [UserController::class, 'show']);
+Route::post('/users', [UserController::class, 'store']);
+Route::patch('/users/{user}', [UserController::class, 'update']);
+Route::delete('/users/{user}', [UserController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
