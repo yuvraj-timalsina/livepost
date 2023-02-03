cc7fd985f82c5e28d796d0e12c9256d2f6f49030
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-02-03 17:10:00 +0545

    API Docs

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,9 +8,24 @@
     use App\Repositories\PostRepository;
     use Illuminate\Http\JsonResponse;
     use Illuminate\Http\Request;
+    use Illuminate\Http\Resources\Json\ResourceCollection;
 
+    /**
+     * @group Post Management
+     * APIs to manage Post.
+     */
     class PostController extends Controller
     {
+        /**
+         * Display a listing of posts.
+         * Gets a list of posts.
+         *
+         * @queryParam page_size int Size per page. Defaults to 20. Example: 20
+         * @queryParam page int Page to view. Example: 1
+         * @apiResourceCollection App\Http\Resources\PostResource
+         * @apiResourceModel App\Models\Post
+         * @return ResourceCollection
+         */
         public function index(Request $request)
         {
             $pageSize = $request->page_size ?? 20;
@@ -19,6 +34,20 @@
             return PostResource::collection($posts);
         }
 
+        /**
+         * Store a newly created post in storage.
+         *
+         * @bodyParam title string required Title of the post. Example: Amazing Post
+         * @bodyParam body string[] required Body of the post. Example: ["This post is super beautiful"]
+         * @bodyParam user_ids int[] required The author ids of the post. Example: [1, 2]
+         * @apiResource App\Http\Resources\PostResource
+         * @apiResourceModel App\Models\Post
+         *
+         * @param PostStoreRequest $request
+         * @param \App\Repositories\PostRepository $repository
+         *
+         * @return PostResource
+         */
         public function store(PostStoreRequest $request, PostRepository $repository)
         {
             $created = $repository->create($request->validate());
@@ -26,11 +55,36 @@
             return new PostResource($created);
         }
 
+        /**
+         * Display the specified post.
+         *
+         * @apiResource App\Http\Resources\PostResource
+         * @apiResourceModel App\Models\Post
+         *
+         * @param \App\Models\Post $post
+         *
+         * @return PostResource
+         */
         public function show(Post $post)
         {
             return new PostResource($post);
         }
 
+        /**
+         * Update the specified post in storage.
+         *
+         * @bodyParam title string required Title of the post. Example: Amazing Post
+         * @bodyParam body string[] required Body of the post. Example: ["This post is super beautiful"]
+         * @bodyParam user_ids int[] required The author ids of the post. Example: [1, 2]
+         * @apiResource App\Http\Resources\PostResource
+         * @apiResourceModel App\Models\Post
+         *
+         * @param \Illuminate\Http\Request $request
+         * @param \App\Models\Post $post
+         * @param \App\Repositories\PostRepository $repository
+         *
+         * @return PostResource | JsonResponse
+         */
         public function update(Request $request, Post $post, PostRepository $repository)
         {
             $post = $repository->update($post, $request->only([
@@ -42,6 +96,18 @@
             return new PostResource($post);
         }
 
+        /**
+         * Remove the specified post from storage.
+         *
+         * @response 200 {
+         * "data": "success"
+         * }
+         *
+         * @param \App\Models\Post $post
+         * @param \App\Repositories\PostRepository $repository
+         *
+         * @return \Illuminate\Http\JsonResponse
+         */
         public function destroy(Post $post, PostRepository $repository)
         {
             $post = $repository->forceDelete($post);
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,9 +8,24 @@
     use App\Repositories\UserRepository;
     use Illuminate\Http\JsonResponse;
     use Illuminate\Http\Request;
+    use Illuminate\Http\Resources\Json\ResourceCollection;
 
+    /**
+     * @group User Management
+     * APIs to manage Users.
+     * */
     class UserController extends Controller
     {
+        /**
+         * Display a listing of users.
+         * Gets a list of users.
+         *
+         * @queryParam page_size int Size per page. Defaults to 20. Example: 20
+         * @queryParam page int Page to view. Example: 1
+         * @apiResourceCollection App\Http\Resources\UserResource
+         * @apiResourceModel App\Models\User
+         * @return ResourceCollection
+         */
         public function index(Request $request)
         {
             event(new UserCreated(uUser::factory()->make()));
@@ -19,6 +34,19 @@
             return UserResource::collection($users);
         }
 
+        /**
+         * Store a newly created resource in storage.
+         *
+         * @bodyParam name string required Name of the user. Example: John Doe
+         * @bodyParam email string required Email of the user. Example: doe@doe.com
+         * @apiResource App\Http\Resources\UserResource
+         * @apiResourceModel App\Models\User
+         *
+         * @param \Illuminate\Http\Request $request
+         * @param \App\Repositories\UserRepository $repository
+         *
+         * @return UserResource
+         */
         public function store(Request $request, UserRepository $repository)
         {
             $created = $repository->create($request->only([
@@ -30,11 +58,36 @@
             return new UserResource($created);
         }
 
+        /**
+         * Display the specified user.
+         *
+         * @urlParam id int required User ID
+         * @apiResource App\Http\Resources\UserResource
+         * @apiResourceModel App\Models\User
+         *
+         * @param \App\Models\User $user
+         *
+         * @return UserResource
+         */
         public function show(User $user)
         {
             return new UserResource($user);
         }
 
+        /**
+         * Update the specified user in storage.
+         *
+         * @bodyParam name string Name of the user. Example: John Doe
+         * @bodyParam email string Email of the user. Example: doe@doe.com
+         * @apiResource App\Http\Resources\UserResource
+         * @apiResourceModel App\Models\User
+         *
+         * @param \Illuminate\Http\Request $request
+         * @param \App\Models\User $user
+         * @param \App\Repositories\UserRepository $repository
+         *
+         * @return UserResource | JsonResponse
+         */
         public function update(Request $request, User $user, UserRepository $repository)
         {
             $user = $repository->update($user, $request->only([
@@ -45,6 +98,18 @@
             return new UserResource($user);
         }
 
+        /**
+         * Remove the specified user from storage.
+         *
+         * @response 200 {
+         * "data": "success"
+         * }
+         *
+         * @param \App\Models\User $user
+         * @param \App\Repositories\UserRepository $repository
+         *
+         * @return \Illuminate\Http\JsonResponse
+         */
         public function destroy(User $user, UserRepository $repository)
         {
             $deleted = $repository->forceDelete($user);
