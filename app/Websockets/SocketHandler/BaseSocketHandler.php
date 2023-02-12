a0d8df1029922707d52c812f264909c2c51a1925
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-02-12 22:55:12 +0545

    Sending realtime data w/ RPC

---
+++ app/Websockets/SocketHandler/BaseSocketHandler.php
@@ -0,0 +1,54 @@
+<?php
+
+    namespace App\Websockets\SocketHandler;
+
+    use BeyondCode\LaravelWebSockets\Apps\App;
+    use BeyondCode\LaravelWebSockets\QueryParameters;
+    use BeyondCode\LaravelWebSockets\WebSockets\Exceptions\UnknownAppKey;
+    use Ratchet\ConnectionInterface;
+    use Ratchet\WebSocket\MessageComponentInterface;
+
+    abstract class BaseSocketHandler implements MessageComponentInterface
+    {
+        function onOpen(ConnectionInterface $conn)
+        {
+            dump('on opened');
+
+//        auth logic here
+
+            $this->verifyAppKey($conn)->generateSocketId($conn);
+        }
+
+        protected function generateSocketId(ConnectionInterface $connection)
+        {
+            $socketId = sprintf('%d.%d', random_int(1, 1000000000), random_int(1, 1000000000));
+
+            $connection->socketId = $socketId;
+
+            return $this;
+        }
+
+        protected function verifyAppKey(ConnectionInterface $connection)
+        {
+            $appKey = QueryParameters::create($connection->httpRequest)->get('appKey');
+
+            if (!$app = App::findByKey($appKey)) {
+                throw new UnknownAppKey($appKey);
+            }
+
+            $connection->app = $app;
+
+            return $this;
+        }
+
+        function onClose(ConnectionInterface $conn)
+        {
+            dump('closed');
+        }
+
+        function onError(ConnectionInterface $conn, \Exception $e)
+        {
+            dump($e);
+            dump('onerror');
+        }
+    }
